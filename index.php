b9a594aaf570548c2000c1115cb8c1598a3dcace
NaiuGe <naiaragenova@gmail.com>
2024-09-25 01:43:07 -0300

    Actualización Index

--- index.php
+++ index.php
@@ -14,51 +14,52 @@
 <body>
     <div class="grupo">
         <aside class="perfil" href=""> 
-            <img src="logo perfil.png" id="fotoperfil">
+            <img src="imagenes/icono_usuario.png" id="fotoperfil">
             <?php
             if(isset($_SESSION['contador'])){
                 echo '<br></br>';
                 echo '<b>Bienvenido '. $_SESSION['nombre'].'</b>';
                 echo '<br></br>';
                 echo "<input type='button' value='cerrar sesion' onclick='location=\"cerrarlogin.php\"'>";
-            } else {echo '<br></br><input type="button" value="Iniciar sesion" onclick="location=\'login.html\'">';}
+            } else {echo '<br><input type="button" value="Iniciar sesion" onclick="location=\'login.html\'">';}
             ?>      
         </aside>
     <header class="cabeceraindex">
         <h1>Ponete a laburar</h1>
         <form>
-            <input type="search" name="busq" class="caja">
+            <input type="search" name="busq" class="caja" placeholder="Buscar profesión">
             <input type="submit" value="Enviar" class="boton">
         </form>
     </header>
+    
     <div class="seccion">
         <div class="publicaciones"> 
-            <a href="">
+            <a href="" class="link">
                 <img src="imagenes/icono_trabajo.png" id="fotopubli">
             </a>
         </div>
         <div class="publicaciones"> 
-            <a href="">
+            <a href="" class="link">
                 <img src="imagenes/icono_trabajo.png" id="fotopubli">
             </a>
         </div>
         <div class="publicaciones"> 
-            <a href="">
+            <a href="" class="link">
                 <img src="imagenes/icono_trabajo.png" id="fotopubli">
             </a>
         </div>
         <div class="publicaciones"> 
-            <a href="">
+            <a href="" class="link">
                 <img src="imagenes/icono_trabajo.png" id="fotopubli">
             </a>
         </div>
         <div class="publicaciones"> 
-            <a href="">
+            <a href="" class="link">
                 <img src="imagenes/icono_trabajo.png" id="fotopubli">
             </a>
         </div>
         <div class="publicaciones"> 
-            <a href="">
+            <a href="" class="link">
                 <img src="imagenes/icono_trabajo.png" id="fotopubli">
             </a>
         </div>
