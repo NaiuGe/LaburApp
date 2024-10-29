34df6d4cc053e52461e71ba270894cf751a5966d
gerogerardo <andrescaporale11@gmail.com>
2024-10-29 19:36:36 -0300

    Update info_perfil.php

--- info_perfil.php
+++ info_perfil.php
@@ -14,7 +14,7 @@
 
     $id_usuario = $_SESSION['id_usuario'];
 
-    $consulta = "SELECT usuarios.* FROM usuarios WHERE usuarios.id_usuario = '$id_usuario';";
+    $consulta = "SELECT * FROM usuarios WHERE id_usuario = '$id_usuario';";
 
     $resultado = mysqli_query($conexion, $consulta);
 
@@ -70,11 +70,11 @@
         echo "<h3> Domicilio </h3>";
         echo "<input type='text' name='domicilio' value='".$row[4]."'>";
         echo "<h3> CONTRASEÑA </h3>";
-        echo "<input type='text' name='pass' value='".$row[7]."'>";
+        echo "<input type='text' name='pass' value='".$row[6]."'>";
         echo "<h3> Número de Telefono </h3>";
-        echo "<input type='tel' name='telefono' value='".$row[8]."'>";
+        echo "<input type='tel' name='telefono' value='".$row[7]."'>";
         echo "<h3> Descripción personal </h3>";
-        echo "<input type='text' name='informacion' value='".$row[9]."'>";
+        echo "<input type='text' name='informacion' value='".$row[8]."'>";
         echo "<br><br>";
         echo"<input class='boton' type='submit' value='Enviar'> ";
         echo "&nbsp;&nbsp;&nbsp;&nbsp;";
