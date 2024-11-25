15116e5690790e9831c080a2afea7e4f2a2de275
YoelPierucciBazzo <pbyoel2001@gmail.com>
2024-11-25 16:04:59 -0300

    Cambios en el gráfico

--- generarGrafico.php
+++ generarGrafico.php
@@ -4,19 +4,29 @@ require('fpdf/fpdf.php');
 date_default_timezone_set("America/Argentina/Buenos_Aires"); 
 
 $pdf = new FPDF();
-
 $pdf->AddPage('L');
 
 $pdf->SetFont('Arial', 'B', 18);
-$pdf->Cell(0,30,utf8_decode('Porcentaje de personas por cada localidad'),0,1,'C',0);
+$pdf->Cell(0, 30, utf8_decode('Porcentaje de personas por cada localidad'), 0, 1, 'C', 0);
 
-$grafico = $_POST['imagen']; 
 
-$img = explode(',', $grafico, 2)[1];  
+$grafico1 = $_POST['imagen1'];
+$img1 = explode(',', $grafico1, 2)[1];  
+$pic1 = 'data://text/plain;base64,' . $img1;
+$pdf->Image($pic1, 20, 50, 240, 0, 'PNG');
 
-$pic = 'data:text/plain;base64,'. $img;
 
-    $pdf->image($pic,20,50,300,0,'png'); 
+$pdf->Ln(90);
+$pdf->AddPage('L');
 
-    $pdf->Output();
-    ?>
+$pdf->SetFont('Arial', 'B', 18);
+$pdf->Cell(0, 30, utf8_decode('Número de publicaciones por profesión'), 0, 1, 'C', 0);
+
+$grafico2 = $_POST['imagen2'];
+$img2 = explode(',', $grafico2, 2)[1];  
+$pic2 = 'data://text/plain;base64,' . $img2;
+$pdf->Image($pic2, 20, 50, 240, 0, 'PNG');
+
+
+$pdf->Output();
+?>
